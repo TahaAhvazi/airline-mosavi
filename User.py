d1565aa2d46dba284cfe02417aebee0f71d74f74
MrThink26 <95047019+MrThink26@users.noreply.github.com>
2024-08-21 21:51:49 +0330

    User class

---
+++ User.py
@@ -0,0 +1,53 @@
+import bcrypt
+import jwt
+import datetime
+
+class User:
+    SECRET_KEY = 'secret_key'  
+
+    def init(self, username, password, name, email, phone_number):
+        self.username = username
+        self.__password_hash = self.hash_password(password)
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+        self.is_logged_in = False
+
+    def hash_password(self, password):
+        
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    def check_password(self, password):
+        
+        return bcrypt.checkpw(password.encode(), self.__password_hash)
+
+    def create_token(self):
+        
+        payload = {
+            'username': self.username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)  
+        }
+        return jwt.encode(payload, self.SECRET_KEY, algorithm='HS256')
+
+    def login(self, password):
+       
+        if self.check_password(password):
+            self.is_logged_in = True
+            return self.create_token()
+        return None
+
+    def logout(self):
+        
+        self.is_logged_in = False
+
+    def display_info(self):
+        
+        if self.is_logged_in:
+            return {
+                "Username": self.username,
+                "Name": self.name,
+                "Email": self.email,
+                "Phone Number": self.phone_number
+            }
+        else:
+            return "User is not logged in."
